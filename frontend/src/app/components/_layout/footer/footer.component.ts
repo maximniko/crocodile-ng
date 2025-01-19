db37db5798bcd2ab999041eb844b282a93e98fcc
Maxim Niko <m.nikohdiuk@gmail.com>
2025-01-19 23:09:07 +0200

    Fix players form (#2)
    * start project (beta)
    
    * start project (beta)
    
    * start project (beta)
    
    * start project (beta)
    
    * start project
    
    * fix players form
    
    ---------
    
    Co-authored-by: maxim.niko <m.nikohdiuk@autodoc.eu>

--- frontend/src/app/components/_layout/footer/footer.component.ts
+++ frontend/src/app/components/_layout/footer/footer.component.ts
@@ -4,11 +4,11 @@ import {Component} from '@angular/core';
   selector: 'app-footer',
   standalone: true,
   template: `
-    <footer class="footer py-3 bg-body-tertiary">
-      <div class="container">
-        <span class="text-body-secondary">Place sticky footer content here.</span>
-      </div>
-    </footer>
+<!--    <footer class="footer py-3 bg-body-tertiary">-->
+<!--      <div class="container">-->
+<!--        <span class="text-body-secondary">Place sticky footer content here.</span>-->
+<!--      </div>-->
+<!--    </footer>-->
   `,
 })
 export class FooterComponent {
--- frontend/src/app/components/_layout/header/header.component.ts
+++ frontend/src/app/components/_layout/header/header.component.ts
@@ -1,5 +1,4 @@
-import {Component, inject, signal, TemplateRef, WritableSignal} from '@angular/core';
-import {NgbOffcanvas, OffcanvasDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import {Component} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {routeCreator} from '../../crocodile/crocodile.routes';
 
@@ -7,41 +6,8 @@ import {routeCreator} from '../../crocodile/crocodile.routes';
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
-  imports: [
-    RouterLink
-  ]
+  imports: [RouterLink]
 })
 export class HeaderComponent {
-  private offcanvasService = inject(NgbOffcanvas);
-  closeResult: WritableSignal<string> = signal('');
-
-  open(content: TemplateRef<any>) {
-    this.offcanvasService.open(
-      content,
-      {
-        ariaLabelledBy: 'offcanvas-basic-title',
-        position: 'end'
-      }
-    ).result.then(
-      (result) => {
-        this.closeResult.set(`Closed with: ${result}`);
-      },
-      (reason) => {
-        this.closeResult.set(`Dismissed ${this.getDismissReason(reason)}`);
-      },
-    );
-  }
-
-  private getDismissReason(reason: any): string {
-    switch (reason) {
-      case OffcanvasDismissReasons.ESC:
-        return 'by pressing ESC';
-      case OffcanvasDismissReasons.BACKDROP_CLICK:
-        return 'by clicking on the backdrop';
-      default:
-        return `with: ${reason}`;
-    }
-  }
-
   protected readonly routeCreator = routeCreator;
 }
