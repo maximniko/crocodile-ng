4729331fa35fafdfb5a41679c377676f20e1f86c
maxim.niko <m.nikohdiuk@autodoc.eu>
2025-02-05 11:24:02 +0200

    fix ios keyboard

--- frontend/src/app/components/crocodile/components/players/players.component.ts
+++ frontend/src/app/components/crocodile/components/players/players.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {
   AbstractControl,
   FormArray,
@@ -50,7 +50,7 @@ import {TwaService} from '../../../../services/telegram/twa.service';
   host: {class: 'd-flex flex-column gap-2 pb-5'},
   imports: [ReactiveFormsModule, FocusOnShowDirective],
 })
-export class PlayersComponent implements OnInit, OnDestroy {
+export class PlayersComponent {
   protected form!: FormGroup;
   protected startPlayers: Player[] = [];
 
@@ -58,18 +58,9 @@ export class PlayersComponent implements OnInit, OnDestroy {
     protected formBuilder: FormBuilder,
     protected playersService: PlayersService,
     private router: Router,
-    private twa: TwaService,
   ) {
     this.startPlayers = this.playersService.players
     this.form = this.makeForm(this.startPlayers)
-  }
-
-  ngOnInit() {
-    this.twa.requestFullscreen()
-  }
-
-  ngOnDestroy() {
-    this.twa.exitFullscreen()
   }
 
   protected get players() {
