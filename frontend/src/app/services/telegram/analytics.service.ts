5641245ed11ba2f9639400530f59a427185973ef
maxim.niko <m.nikohdiuk@autodoc.eu>
2025-02-16 17:42:58 +0200

    add analytics

---
+++ frontend/src/app/services/telegram/analytics.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from '@angular/core';
+import TelegramAnalytics from '@telegram-apps/analytics'
+import {environment} from '../../../environments/environment';
+
+@Injectable({providedIn: 'root'})
+export class AnalyticsService {
+    init() {
+        return TelegramAnalytics.init({
+            token: 'eyJhcHBfbmFtZSI6Ik1yRGltYSIsImFwcF91cmwiOiJodHRwczovL3QubWUvY3JvYnJvX2JvdCIsImFwcF9kb21haW4iOiJodHRwczovL3QubWUvY3JvYnJvX2JvdD9wcm9maWxlIn0=!7ZdBQ21fDF7CW9gOjdMbBJRngizmYSte3XyvyR1bEKg=',
+            appName: 'MrDima',
+            env: environment.production ? 'PROD' : 'STG',
+        });
+    }
+}
