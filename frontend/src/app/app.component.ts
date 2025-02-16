f95a9e3ca7296d0be6f1cf787f9f8891066394e8
Maxim Niko <m.nikohdiuk@gmail.com>
2025-02-16 18:00:09 +0200

    Analytics (#5)
    * add analytics
    
    * add analytics
    
    ---------
    
    Co-authored-by: maxim.niko <m.nikohdiuk@autodoc.eu>

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,6 +3,7 @@ import {RouterOutlet} from '@angular/router';
 import {TwaService} from './services/telegram/twa.service';
 import {SymbolsComponent} from './components/_layout/symbols/symbols.component';
 import {PlayersService} from './components/crocodile/services/players/players.service';
+import {AnalyticsService} from './services/telegram/analytics.service';
 
 @Component({
   selector: 'app-root',
@@ -16,13 +17,23 @@ import {PlayersService} from './components/crocodile/services/players/players.se
 export class AppComponent implements OnInit, OnDestroy {
   constructor(
     private twa: TwaService,
+    private analytics: AnalyticsService,
     private playersService: PlayersService
   ) {
   }
 
   ngOnInit() {
-    this.twa.ready()
-    this.twa.expand()
+    try {
+      this.analytics.init()
+        .then(
+          (value: void) => console.log(`analytics inited successful ${value}`),
+          (reason) => console.log(`analytics inited unsuccessful ${reason}`)
+        )
+      this.twa.ready()
+      this.twa.expand()
+    } catch (e) {
+      console.error((e as Error)?.message)
+    }
   }
 
   ngOnDestroy() {
