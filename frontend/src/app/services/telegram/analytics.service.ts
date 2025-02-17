46cd48715f2f6478ae55aa211d98355bc7c4809a
maxim.niko <m.nikohdiuk@autodoc.eu>
2025-02-17 20:26:49 +0200

    change appName

--- frontend/src/app/services/telegram/analytics.service.ts
+++ frontend/src/app/services/telegram/analytics.service.ts
@@ -7,7 +7,7 @@ export class AnalyticsService {
     init() {
         return TelegramAnalytics.init({
             token: 'eyJhcHBfbmFtZSI6Ik1yRGltYSIsImFwcF91cmwiOiJodHRwczovL3QubWUvY3JvYnJvX2JvdCIsImFwcF9kb21haW4iOiJodHRwczovL3QubWUvY3JvYnJvX2JvdD9wcm9maWxlIn0=!7ZdBQ21fDF7CW9gOjdMbBJRngizmYSte3XyvyR1bEKg=',
-            appName: 'Crocodile',
+            appName: 'MrDima',
             env: environment.production ? 'PROD' : 'STG',
         });
     }
